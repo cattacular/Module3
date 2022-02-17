939e1fb75bc7064feeb2edb17027df331cf5b589
Matt Bigge <69949135+MattBigge@users.noreply.github.com>
2022-02-17 15:57:17 -0600

    my push

--- Assets/Scripts/spikeKill.cs
+++ Assets/Scripts/spikeKill.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class spikeKill : MonoBehaviour
 {
+    public GameObject skull;
     // Start is called before the first frame update
 
     void OnTriggerEnter(Collider other)
     {
         other.gameObject.SetActive(false);
+        Instantiate(skull, this.transform);
     }
 }
